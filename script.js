03d783fc971cc6fa9e9952648efaeeb4c1923e33
Chandu Kiran <chandukiran13@gmail.com>
2021-08-21 12:06:08 +0530

    Beautify the code

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const locationElement = document.querySelector("h5");
 const weather = {};
 
 weather.temp = {
-    unit : "celsius"
+    unit: "celsius"
 }
 
 const KELVIN = 273;
@@ -13,40 +13,40 @@ const KELVIN = 273;
 const key = "8e2487f06e40e18330ac4adc6c2e5e45";
 
 getWeather("nandyal");
-function getWeather(city_name){
+
+function getWeather(city_name) {
     let api = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${key}`;
-    
+
     fetch(api)
-        .then(function(response){
+        .then(function (response) {
             let data = response.json();
             return data;
         })
-        .then(function(data){
+        .then(function (data) {
             weather.temp.value = Math.floor(data.main.temp - KELVIN);
             weather.description = data.weather[0].description;
             weather.city = data.name;
             weather.country = data.sys.country;
         })
-        .then(function(){
+        .then(function () {
             displayWeather();
         });
 }
 
-function displayWeather(){
+function displayWeather() {
     tempElement.innerHTML = `${weather.temp.value}°<span>C</span>`;
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
 
 
-function celsiusToFahrenheit(){
-    if (weather.temp.unit == "celsius"){
-        let fahrenheit = (weather.temp.value * 9/5) + 32;
+function celsiusToFahrenheit() {
+    if (weather.temp.unit == "celsius") {
+        let fahrenheit = (weather.temp.value * 9 / 5) + 32;
         tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
         weather.temp.unit = "fahrenheit"
-    }else{
+    } else {
         tempElement.innerHTML = `${weather.temp.value}°<span>C</span>`;
         weather.temp.unit = "celsius"
     }
 }
-
